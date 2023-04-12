961c291f60be76c16cd4a83552e5be1b063bb47b
crusher8010 <crusher8010@gmail.com>
2023-04-12 23:37:12 +0530

    removed validation

--- api/users/userRouter.js
+++ api/users/userRouter.js
@@ -2,8 +2,8 @@ const { createUser, getUsers, SingleUser, UpdateUser, RemoveUser, Login } = requ
 const { Check } = require("../../auth/validation");
 const router = require("express").Router();
 
-router.route("/").get(Check, getUsers).post(createUser);
+router.route("/").get(getUsers).post(createUser);
 router.route("/login").post(Login);
-router.route("/:id").get(Check, SingleUser).patch(Check, UpdateUser).delete(Check, RemoveUser);
+router.route("/:id").get(SingleUser).patch(Check, UpdateUser).delete(Check, RemoveUser);
 
 module.exports = router;
